a894cf0e80f8d6b32b15832839cd22ec19ca1d91
João Guedes <jguedes2408@gmail.com>
2026-06-10 19:53:54 -0300

    criado o arquivo js

---
+++ js/script_fundamentos.js
@@ -0,0 +1,21 @@
+//Comentário de uma linha
+
+/*
+    Comentários de múltiplas linhas
+    É possivel ter várias 
+    linhas
+
+*/
+
+//COMANDO DE SAÍDA
+console.log("Olá mundo!!!")
+
+//DECLARAÇÃO DE VARIÁVEL
+let num = 10
+var num2 = 50
+const num3 = 100
+
+
+console.log(num)
+console.log(num2)
+console.log(num3)
